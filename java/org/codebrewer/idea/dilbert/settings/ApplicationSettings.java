c101fbbdf1306d879146635d11deb04292d0ef91
Mark Scott <mark@codebrewer.org>
2005-10-10 22:19:23 +0000

    Removed setters for configurable properties and provided a constructor to initialise them instead.

--- java/org/codebrewer/idea/dilbert/settings/ApplicationSettings.java
+++ java/org/codebrewer/idea/dilbert/settings/ApplicationSettings.java
@@ -30,6 +30,7 @@ import org.jdom.Element;
  *
  * @author Mark Scott
  * @version $Revision$ $Date$
+ * @noinspection NonFinalFieldReferencedInHashCode,NonFinalFieldReferenceInEquals
  */
 public final class ApplicationSettings implements JDOMExternalizable
 {
@@ -38,9 +39,9 @@ public final class ApplicationSettings implements JDOMExternalizable
   private static final String LOAD_STRIP_ON_STARTUP_KEY = "loadStripOnStartup";
   private static final String REFRESH_ALL_PROJECTS_KEY = "refreshAllOpenProjects";
 
-  private boolean disclaimerAcknowledged = false;
-  private boolean loadStripOnStartup = false;
-  private boolean refreshAllOpenProjects = false;
+  private boolean disclaimerAcknowledged;
+  private boolean loadStripOnStartup;
+  private boolean refreshAllOpenProjects;
 
   /**
    * Default constructor that creates an instance having configuration options
@@ -48,7 +49,28 @@ public final class ApplicationSettings implements JDOMExternalizable
    */
   public ApplicationSettings()
   {
+    this(false, false, false);
     LOGGER.debug("ApplicationSettings()");
+  }
+
+  /**
+   * Creates an instance having configuration options (boolean properties) as
+   * specified.
+   *
+   * @param disclaimerAcknowledged has the user acknowledged the plugin's
+   * disclaimer?
+   * @param loadStripOnStartup should the strip be fetched automatically when a
+   * project is opened?
+   * @param refreshAllOpenProjects should all open projects be refreshed when
+   * the strip is fetched for a single projec?
+   */
+  public ApplicationSettings(boolean disclaimerAcknowledged,
+                             boolean loadStripOnStartup,
+                             boolean refreshAllOpenProjects)
+  {
+    this.disclaimerAcknowledged = disclaimerAcknowledged;
+    this.loadStripOnStartup = loadStripOnStartup;
+    this.refreshAllOpenProjects = refreshAllOpenProjects;
   }
 
   /**
@@ -87,48 +109,6 @@ public final class ApplicationSettings implements JDOMExternalizable
     return refreshAllOpenProjects;
   }
 
-  /**
-   * Sets whether or not the user has acknowledged the plugin's disclaimer.
-   *
-   * @param disclaimerAcknowledged <code>true</code> if the user has
-   *                               acknowledged the plugin's disclaimer,
-   *                               <code>false</code> if not.
-   */
-  public void setDisclaimerAcknowledged(final boolean disclaimerAcknowledged)
-  {
-    this.disclaimerAcknowledged = disclaimerAcknowledged;
-  }
-
-  /**
-   * Sets whether or not the plugin should automatically download the current
-   * daily strip when a project is opened or should only do so when the user
-   * explicitly requests it.
-   *
-   * @param loadStripOnStartup <code>true</code> if the current daily strip
-   *                           should be fetched as soon as a project is opened,
-   *                           <code>false</code> if not.
-   */
-  public void setLoadStripOnStartup(final boolean loadStripOnStartup)
-  {
-    this.loadStripOnStartup = loadStripOnStartup;
-  }
-
-  /**
-   * Sets whether or not refreshing the plugin's toolwindow in one project
-   * should cause all other open projects to be refreshed as well.
-   *
-   * @param refreshAllOpenProjects <code>true</code> if updating one project
-   *                               should cause all other projects to be updated,
-   *                               <code>false</code> if not.
-   */
-  public void setRefreshAllOpenProjects(final boolean refreshAllOpenProjects)
-  {
-    this.refreshAllOpenProjects = refreshAllOpenProjects;
-  }
-
-  /**
-   * @noinspection MethodWithMultipleReturnPoints,NonFinalFieldReferenceInEquals
-   */
   public boolean equals(final Object o)
   {
     if (o == null) {
@@ -153,13 +133,9 @@ public final class ApplicationSettings implements JDOMExternalizable
     return refreshAllOpenProjects == settings.refreshAllOpenProjects;
   }
 
-  /**
-   * @noinspection NonFinalFieldReferencedInHashCode
-   */
   public int hashCode()
   {
-    int result;
-    result = disclaimerAcknowledged ? 1 : 0;
+    int result = disclaimerAcknowledged ? 1 : 0;
     result = 29 * result + (loadStripOnStartup ? 1 : 0);
     result = 29 * result + (refreshAllOpenProjects ? 1 : 0);
 
